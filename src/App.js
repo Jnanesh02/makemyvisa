60e1da01c2518704f6029f087bb400573c0d341d
Ratnam Solutions <manikanta@ratnamsolutions.com>
2023-12-29 18:53:38 +0530

    created navBar and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Pages/Login';
+import Registration from './Pages/Registration';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       welcome
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/login' exact element={<Login/>}/> 
+          <Route path='/registration' exact element={<Registration/>}/> 
+        </Routes>
+      </Router>
     </div>
   );
 }
