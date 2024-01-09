4dfd31a555d7ae1691b7352f727757cfa981d58e
Jnanesh02 <jnanesh02@rantnamsolutions.com>
2024-01-09 11:02:34 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './Pages/Login/Login';
-import Layout from './Pages/Layout';
-import Home from './Pages/Home/Home';
-import SignUp from './Pages/Registraion/SignUp';
-import Dashboard from './Pages/Dashboard/Dashboard';
+import Login from "./Pages/Login/Login";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home/Home";
+import SignUp from "./Pages/Registraion/SignUp";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 
 function App() {
+  const Visa = () => <h1>visa is working</h1>;
+  const Travel = () => <h1>travel is working</h1>;
+  const AA = () => <h1>AA is working</h1>;
+  const ContactUs = () => <h1>contactus is working</h1>;
+  const Study = () => <h1>study is working</h1>;
   return (
     <div className="App">
-     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path="login" element={<Login />} />
-          <Route path="registration" element={<SignUp />} />
-          
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="registration" element={<SignUp />} />
           </Route>
-          <Route path='dashboard' element={<Dashboard/>}/>
-         </Routes>
-         </Router>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="visa" element={<Visa />} />
+          <Route path="study" element={<Study />} />
+          <Route path="aa" element={<AA />} />
+          <Route path="contactus" element={<ContactUs />} />
+          <Route path="travel" element={<Travel />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Pages/NavBar/Navbar.js
+++ src/Pages/NavBar/Navbar.js
@@ -1,67 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/images/logo.png";
 import avatar from "../../assets/images/avatar.png";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+// import Dashboard from "../Dashboard/Dashboard";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const Login = () => {
+    return (
+      <Link className="dropdown-item" to="/login">
+        Login
+      </Link>
+    );
+  };
+  const Logout = () => {
+    return (
+      <Link className="dropdown-item" to="/">
+        Logout
+      </Link>
+    );
+  };
+  const DashboardLink = () => {
+    return (
+      <Link className="dropdown-item" to="/dashboard">
+        Dashboard
+      </Link>
+    );
+  };
+  const Signup = () => {
+    return (
+      <Link className="dropdown-item" to="/registration">
+        signup
+      </Link>
+    );
+  };
   return (
     <main className="content">
-    <header>
-      <section className="header-section">
+      <header>
+        <section className="header-section">
           <div className="container-fluid">
-              <div className="row">
-                  <div className="col-lg-2 col-sm-4 col-md-4 col">
-                  <img className="header-logo" src={Logo} alt="Logo" />
-
-                  </div>
-                  <div className="col-lg-10 col-sm-8 col-md-8  nav-cont-b">
-                      <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-section">
-                          <div className="container-fluid">
-                            
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                              <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                              <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-list">
-                                <li className="nav-item">
-                                  <a className="nav-link active" aria-current="page" href="/"> Visa </a>
-                                </li>
-                                <li className="nav-item">
-                                  <a className="nav-link" href="/"> Travel ItineraryInsurance</a>
-                                </li>
-                                <li className="nav-item">
-                                  <a className="nav-link" href="/"> Apostille & Attestation </a>
-                                </li>
-                                <li className="nav-item">
-                                  <a className="nav-link" href="/"> Study/Admission </a>
-                                </li>
-                                <li className="nav-item">
-                                  <a className="nav-link" href="/"> Contact Us </a>
-                                </li>
-                             
-                              </ul>
-                              <div className="dropdown drp-navbar">
-                                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src={avatar} alt="Avatar" />
-                                </button>
-                                <ul className="dropdown-menu">
-                                  <li><a className="dropdown-item" href="/">Action</a></li>
-                                  <li><a className="dropdown-item" href="/">Another action</a></li>
-                                  <li><a className="dropdown-item" href="/">Something else here</a></li>
-                                </ul>
-                              </div>
-                              
-                            </div>
-                          </div>
-                        </nav>
-                        
-                  </div>
+            <div className="row">
+              <div className="col-lg-2 col-sm-4 col-md-4 col">
+                <img className="header-logo" src={Logo} alt="Logo" />
               </div>
+              <div className="col-lg-10 col-sm-8 col-md-8  nav-cont-b">
+                <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-section">
+                  <div className="container-fluid">
+                    <button
+                      className="navbar-toggler"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                      className="collapse navbar-collapse"
+                      id="navbarSupportedContent">
+                      <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-list">
+                        <li className="nav-item">
+                          <Link
+                            className="nav-link active"
+                            aria-current="page"
+                            to="/visa">
+                            {" "}
+                            Visa{" "}
+                          </Link>
+                        </li>
+                        <li className="nav-item">
+                          <Link className="nav-link" to="/travel">
+                            {" "}
+                            Travel ItineraryInsurance
+                          </Link>
+                        </li>
+                        <li className="nav-item">
+                          <Link className="nav-link" to="/aa">
+                            {" "}
+                            Apostille & Attestation{" "}
+                          </Link>
+                        </li>
+                        <li className="nav-item">
+                          <Link className="nav-link" to="/study">
+                            {" "}
+                            Study/Admission{" "}
+                          </Link>
+                        </li>
+                        <li className="nav-item">
+                          <Link className="nav-link" to="/contactus">
+                            {" "}
+                            Contact Us{" "}
+                          </Link>
+                        </li>
+                      </ul>
+                      <div className="dropdown drp-navbar">
+                        <button
+                          className="btn btn-secondary dropdown-toggle"
+                          type="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false">
+                          <img src={avatar} alt="Avatar" />
+                        </button>
+                        <ul className="dropdown-menu">
+                          <li>
+                            {isLoggedIn ? (
+                              <DashboardLink></DashboardLink>
+                            ) : (
+                              <Signup></Signup>
+                            )}
+                          </li>
+                          <li>
+                            {isLoggedIn ? <Logout></Logout> : <Login></Login>}
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+            </div>
           </div>
-      </section>
-  </header>
-  
-  </main>
+        </section>
+      </header>
+    </main>
   );
 };
 
