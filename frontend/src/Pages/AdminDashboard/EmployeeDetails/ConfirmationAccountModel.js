9536f0ea957b173d449880e7fc61d1af6450693a
Ratnam Solutions <manikanta@ratnamsolutions.com>
2024-01-20 10:11:31 +0530

    confirmation account model

---
+++ frontend/src/Pages/AdminDashboard/EmployeeDetails/ConfirmationAccountModel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./ConfirmationAccountModel.css"
+const ConfirmationAccountModel = ({message,onConfirm,onCancel}) => {
+  return (
+    <div className="confirmation-modal">
+    <p>{message}</p>
+    <button onClick={onConfirm}>Confirm</button>
+    <button onClick={onCancel}>Cancel</button>
+  </div>
+  )
+}
+
+export default ConfirmationAccountModel
