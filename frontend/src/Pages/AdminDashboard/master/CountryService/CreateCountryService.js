1c314312f09415643075246077116bf18cc139c8
Ratnam Solutions <vijay@ratnamsolutions.com>
2024-02-13 10:42:33 +0530

    service

--- frontend/src/Pages/AdminDashboard/master/CountryService/CreateCountryService.js
+++ frontend/src/Pages/AdminDashboard/master/CountryService/CreateCountryService.js
@@ -210,6 +210,7 @@ export const CreateCountryService = ({
   return (
     <>
       <div className="create-account-dashboard create-country">
+        <div className="create-dep-form scrl-frm">
       <button className="close-buttonss" onClick={handleCloseModal}>X</button>
         <div className="create-country-form">
           <div className="country-form-input mb-3">
@@ -356,7 +357,9 @@ export const CreateCountryService = ({
             Cancel
           </button>
         </div>
+        </div>
       </div>
+  
 
       {isConfirmationModalOpen && (
         <ConfirmationModal
