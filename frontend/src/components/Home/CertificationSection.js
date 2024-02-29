33e050c4e908f019622a1b0673c4fbf125dc3265
praveensagar22 <praveensagarbingi@gmail.com>
2024-02-29 17:50:17 +0530

    certification carsoul

--- frontend/src/components/Home/CertificationSection.js
+++ frontend/src/components/Home/CertificationSection.js
@@ -1,37 +1,40 @@
 import React from 'react';
-import certificate1 from '../../assets/images/certificate/certificate-1.jpg';
-import certificate2 from '../../assets/images/certificate/certificate-2.jpg';
-import certificate3 from '../../assets/images/certificate/certificate-3.jpg';
-import certificate4 from '../../assets/images/certificate/certificate-4.jpg';
-import certificate5 from '../../assets/images/certificate/certificate-5.jpg';
-import backgroundImage from "../../assets/images/certificate/relief-plaster-wall.jpg";
-import "./HomeStyles/certificationSection.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import './HomeStyles/certification.css'
 const CertificationSection = () => {
-    return (
-        <>
-           <section className="certificate-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
-    <div className="container-fluid">
-        <div className="row">
-            <div className="col-lg-12">
-                <h2> ACCREDITATION </h2>
-            </div>
-            <div className="row">
-                <div className="col-lg-12 col-md-12 col-sm-12">
-                    <div className="certicate-images-services">
-                        <img className="certificate-img img-fluid" src={certificate1} alt="Certificate 1"/>
-                        <img className="certificate-img img-fluid" src={certificate2} alt="Certificate 2"/>
-                        <img className="certificate-img img-fluid" src={certificate3} alt="Certificate 3"/>
-                        <img className="certificate-img img-fluid" src={certificate4} alt="Certificate 4"/>
-                        <img className="certificate-img img-fluid" src={certificate5} alt="Certificate 5"/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+  const settings = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: <button className="slick-prev" style={{ backgroundColor: 'red' }}>Previous</button>,
+    nextArrow: <button className="slick-next" style={{ backgroundColor: 'red' }}>Next</button>
+  };
 
-        </>
-    );
-}
+  return (
+    <Slider className="autoplay w-50 mx-auto" {...settings}>
+      <div className=''>
+        <img src="https://picsum.photos/300/200?random=1" alt="Slide 1" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/300/200?random=2" alt="Slide 2" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/300/200?random=3" alt="Slide 3" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/300/200?random=4" alt="Slide 4" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/300/200?random=5" alt="Slide 5" />
+      </div>
+    </Slider>
+  );
+};
 
 export default CertificationSection;
+
+
+
